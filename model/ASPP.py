d12399c24c9d05f62eda2f19d341f78722339ef5
liliililliiil <nsa28961@naver.com>
2023-09-24 18:50:53 +0900

    modified

--- model/ASPP.py
+++ model/ASPP.py
@@ -18,23 +18,18 @@ class ASPP(nn.Module):
     def __init__(self, in_channels, out_channels, num_classes):
         super(ASPP, self).__init__()
         
-        # 1 x 1 convolution -> batchnorm -> relu
         self.conv_1x1_1 = nn.Conv2d(in_channels, out_channels, kernel_size = 1)
         self.bn_conv_1x1_1 = nn.BatchNorm2d(out_channels)
         
-        # 3 x 3 convolution w/ rate = 6 -> batchnorm ->relu
         self.conv_3x3_1 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 6, dilation = 6)
         self.bn_conv_3x3_1 = nn.BatchNorm2d(out_channels)
-        
-        # 3 x 3 convolution w/ rate = 12 -> batchnorm -> relu
+       
         self.conv_3x3_2 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 12, dilation = 12)
         self.bn_conv_3x3_2 = nn.BatchNorm2d(out_channels)
         
-        # 3 x 3 convolution w/ rate = 18 -> batchnorm -> relu
         self.conv_3x3_3 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 18, dilation = 18)
         self.bn_conv_3x3_3 = nn.BatchNorm2d(out_channels)
         
-        # AdaptiveAvgPool2d -> 1 x 1 convolution -> batchnorm -> relu
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         
         self.conv_1x1_2 = nn.Conv2d(in_channels, out_channels, kernel_size = 1)
@@ -50,16 +45,9 @@ class ASPP(nn.Module):
         feature_map_h = feature_map.size()[2] # (== h / 16)
         feature_map_w = feature_map.size()[3] # (== w / 16)
         
-        # shape: (batch_size, out_channels, height/output_stride, width/output_stride)
         out_1x1 = F.relu(self.bn_conv_1x1_1(self.conv_1x1_1(feature_map)))
-        
-        # shape: (batch_size, out_channels, height/output_stride, width/output_stride)
         out_3x3_1 = F.relu(self.bn_conv_3x3_1(self.conv_3x3_1(feature_map)))
-        
-        # shape: (batch_size, out_channels, height/output_stride, width/output_stride)
         out_3x3_2 = F.relu(self.bn_conv_3x3_2(self.conv_3x3_2(feature_map)))
-        
-        # shape: (batch_size, out_channels, height/output_stride, width/output_stride)
         out_3x3_3 = F.relu(self.bn_conv_3x3_3(self.conv_3x3_3(feature_map)))
         
         # shape: (batch_size, in_channels, 1, 1)
